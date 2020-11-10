416345d5c01cefd719ce55528f53f0b1f6cb0b7e
Nick Betzlbacher <nbetzlbacher.hcie-m2019@fh-salzburg.ac.at>
2020-11-10 14:42:20 +0100

    test

--- questions.js
+++ questions.js
@@ -2,26 +2,26 @@
 export const QuizQuestions = [
     {
         question: "Wie lautet der zweite Planet in unserem Sonnensystem?",
-        correctAnswer: "b",
         a: "Jupiter",
         b: "Venus",
         c: "Erde",
-        d: "Mars"
+        d: "Mars",
+        correctAnswer: "b",
     },
     {
         question: "Wie hieß der erste Präsident der USA?",
-        correctAnswer: "c",
         a: "Denzel Washington",
         b: "George Bush",
         c: "George Washington",
-        d: "Bill Clinton"
+        d: "Bill Clinton",
+        correctAnswer: "c",
     },
     {
         question: "Welches Unternehmen wurde von Sergey Brin und Larry Page gegründet?",
-        correctAnswer: "a",
         a: "Google",
         b: "Apple",
         c: "Facebook",
-        d: "Microsoft"
+        d: "Microsoft",
+        correctAnswer: "a"
     },
   ];
